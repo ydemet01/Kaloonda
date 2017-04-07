8c6b4c0f531d079227e14fd099f750a469a571e1
ydemet01 <yiannisdemetriades@yianniss-macbook-pro-2.local>
2017-04-07 20:35:42 +0300

    Initial Commit

---
+++ Kaloonda/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Kaloonda
+//
+//  Created by Yiannis Demetriades on 07/04/17.
+//  Copyright © 2017 Yiannis Demetriades - Panayiotis Nicolaou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
